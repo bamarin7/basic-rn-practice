001b0cc5c0b5861f32a0b75364146d2bf4b506a5
Brian Marin Silva <marinbrian.as@gmail.com>
2023-01-20 13:06:00 -0600

    Setting up Button and TouchableOpacity

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # basic-rn-practice
+
+Practicing basic React Native components and Hooks
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,27 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
-  return <Text style={styles.text}>Hey there, friend!</Text>;
+  return (
+    <View>
+      <Text style={styles.text}>Hey there, friend!</Text>
+      <Button
+        title="Go to Components Demo"
+        onPress={() => console.log('Button pressed')}
+      />
+      <TouchableOpacity
+        onPress={() => console.log('List Pressed')}
+      >
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
+    </View>
+  )
 };
 
 const styles = StyleSheet.create({
   text: {
     fontSize: 40,
+    justifyContent: 'center'
   },
 });
 
